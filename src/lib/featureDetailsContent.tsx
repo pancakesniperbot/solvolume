2aa1f1c3d98884e4a2bb13e0deb0c950ed080ed0
pancakesniperbot <sensepin@proton.me>
2025-04-29 17:51:31 -0700

    fix: remove ChartBar icon import that doesn't exist

--- src/lib/featureDetailsContent.tsx
+++ src/lib/featureDetailsContent.tsx
@@ -1,3 +1,5 @@
 import { TrendingUp, Monitor, Shield, Wallet, BarChart2, Users, Target, Rocket, Clock, Award, Zap } from "lucide-react";
+import { SiSolana } from "react-icons/si";
+import type { FeatureDetailContent } from "@/components/FeatureDetailModal";
 
 // ... existing code ... 
